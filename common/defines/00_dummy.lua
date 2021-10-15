42a0136a122f88aee197572fb8e5eea86ab73fb2
Yannick489 <yannickkuerten@hotmail.de>
2021-10-15 14:54:14 +0200

    eee

---
+++ common/defines/00_dummy.lua
@@ -0,0 +1,20 @@
+
+NDefines.NMilitary.BASE_COMBAT_WIDTH = 15
+NDefines.NMilitary.MAX_COMBAT_WIDTH = 55
+NDefines.NMilitary.WAR_LENGTH_DAMAGE_MODIFIER = 0.01				-- How much (by percentage) the damage dealt will be increased each day of the combat
+NDefines.NMilitary.OVERRUN_FACTOR = 10							-- How much stronger one side of a battle must be to cause the other side to be overrun (integer)
+NDefines.NMilitary.OVERRUN_FACTOR_CANNOT_LEAVE = 2				-- Same as OVERRUN_FACTOR, but when combat cannot be left by the combattants. (integer)
+NDefines.NMilitary.CANNOT_RETREAT_DAYS = 12 						-- How many days combat has to last before one side can retreat
+NDefines.NMilitary.DAYS_PER_PHASE = 3								-- How many days each fire and shock phase lasts
+NDefines.NMilitary.COMBAT_DICE_SIDE = 10							-- How many sides the dice used in combat has
+NDefines.NMilitary.LEADER_MAX_PIPS = 6								-- Max general/admiral pips (per skill)
+NDefines.NMilitary.LEADER_MIN_PIPS = 1								-- Min general/admiral pips (total)
+NDefines.NMilitary.LOW_MORALE_THRESHOLD = 0.50					-- Under this percentage of the morale, the army will do a shattered retreat
+NDefines.NMilitary.RESERVE_TICK_DAMAGE = 0.03
+NDefines.NMilitary.INF_REGIMENT_MANPOWER_COST = 1.0 				-- 1 = 1000 men in cost.
+NDefines.NMilitary.CAV_REGIMENT_MANPOWER_COST = 1.0 				-- 1 = 1000 men in cost.
+NDefines.NMilitary.ART_REGIMENT_MANPOWER_COST = 1.0 				-- 1 = 1000 men in cost.
+NDefines.NMilitary.CAVALRY_SPEED = 0.7 							-- _MDEF_CAVALRY_SPEED = 10,
+
+
+
---
+++ descriptor.mod
@@ -0,0 +1,6 @@
+version="1.0"
+tags={
+	"Balance"
+}
+name="gecko experimental"
+supported_version="1.31.6"
